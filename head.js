79a70c1a047abab3a04b26f4ee2302f84f6a55a0
Clifford Tse <TseClifford@gmail.com>
2022-01-10 20:13:49 +0000

    implement head function, returning the first item in an array

---
+++ head.js
@@ -0,0 +1,21 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual}  === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const head = function(array) {
+  if (array.length >= 1) {
+    return array[0];
+  } else {
+    return undefined;
+  }
+};
+
+// TEST CODE
+
+assertEqual(head([5, 6, 7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
