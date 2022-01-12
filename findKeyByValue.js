74307b61d066ba349861dab36066a1fc27499b9d
Clifford Tse <TseClifford@gmail.com>
2022-01-12 22:07:10 +0000

    Implement function that takes a key value and determines the key string from an object.

---
+++ findKeyByValue.js
@@ -0,0 +1,29 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual}  === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire"
+};
+
+const findKeyByValue = function(obj, keyValue) {
+  let key = undefined;
+
+  for (let genre in obj) {
+    if (obj[genre] === keyValue) {
+      key = genre;
+    }
+  }
+  return key;
+};
+
+// TEST CODE
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
