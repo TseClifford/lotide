cef4643d028bfd0e8607186e2c50b65cf0993528
Clifford Tse <TseClifford@gmail.com>
2022-01-13 21:35:24 +0000

    added findKey.js

---
+++ findKey.js
@@ -0,0 +1,31 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion Passed: ${actual}  === ${expected}`);
+  } else if (actual !== expected) {
+    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKey = function(obj, callback) {
+  let keyName = '';
+  for (let key in obj) {
+    if (callback(obj[key])) {
+      keyName = key;
+      break;
+    }
+  }
+  return keyName;
+};
+
+
+assertEqual(
+  findKey({
+    "Blue Hill": { stars: 1 },
+    "Akaleri": { stars: 3 },
+    "noma": { stars: 2 },
+    "elBulli": { stars: 3 },
+    "Ora": { stars: 2 },
+    "Akelarre": { stars: 3 }
+  }, x => x.stars === 2) // => "noma"
+  , 'noma');
