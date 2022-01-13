e1f4fee36ffa97cd5d5cdf34a15e5631e9294a96
Clifford Tse <TseClifford@gmail.com>
2022-01-13 21:23:37 +0000

    added takeUntil.js

---
+++ takeUntil.js
@@ -0,0 +1,38 @@
+const eqArrays = function(firstArray, secondArray) {
+  let eqValue = true;
+  for (let i = 0; i < firstArray.length; i++) {
+    if (firstArray[i] !== secondArray[i]) {
+      eqValue = false;
+    }
+  }
+  return eqValue;
+};
+
+const assertArraysEqual = function(firstArray, secondArray) {
+  if (eqArrays(firstArray, secondArray) === true) {
+    console.log('✅ The two arrays are identical.');
+  } else {
+    console.log('🛑 The two arrays are not identical.');
+  }
+};
+
+const takeUntil = function(array, callback) {
+  let output = [];
+  for (let item of array) {
+    if (callback(item)) {
+      break;
+    }
+    output.push(item);
+  }
+  return output;
+};
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+assertArraysEqual(results1, [ 1, 2, 5, 7, 2 ]);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+assertArraysEqual(results2, [ 'I\'ve', 'been', 'to', 'Hollywood' ]);
