6f5d73065d415169fc8b5f1cb7402c1cc4bdde23
Clifford Tse <TseClifford@gmail.com>
2022-01-17 20:16:22 +0000

    updated tests to use mocha & chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,12 +1,27 @@
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const tail = require('../tail');
 
 // TEST CODE
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+describe("#tail", () => {
+  it(`returns 3 for total elements in original array ["Yo Yo", "Lighthouse", "Labs"]`, () => {
+    const words = ["Yo Yo", "Lighthouse", "Labs"];
+    tail(words); // no need to capture the return value since we are not checking it
+    assert.strictEqual(words.length, 3); // original array should still have 3 elements!
+  });
 
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+  it(`returns 2 for total elements new array`, () => {
+    const result = tail(["Hello", "Lighthouse", "Labs"]);
+    assert.strictEqual(result.length, 2); // original array should still have 3 elements!
+  });
+
+  it(`returns "Lighthouse" for first element in new array`, () => {
+    const result = tail(["Hello", "Lighthouse", "Labs"]);
+    assert.strictEqual(result[0], "Lighthouse"); // original array should still have 3 elements!
+  });
+
+  it(`returns "Labs" for second element in new array`, () => {
+    const result = tail(["Hello", "Lighthouse", "Labs"]);
+    assert.strictEqual(result[1], "Labs"); // original array should still have 3 elements!
+  });
+
+});
