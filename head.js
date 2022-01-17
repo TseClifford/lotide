1486b43288afccd36ec8ad5be3e337cc37d8e5f6
Clifford Tse <TseClifford@gmail.com>
2022-01-17 18:53:13 +0000

    refactored tail function and updated for test

--- head.js
+++ head.js
@@ -1,8 +1,8 @@
-const head = function (array) {
+const head = function(array) {
   if (array.length >= 1) {
     return array[0];
   }
   return undefined;
-}
+};
 
 module.exports = head;
--- tail.js
+++ tail.js
@@ -1,28 +1,10 @@
-// FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual}  === ${expected}`);
-  } else if (actual !== expected) {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const tail = function(array) {
   let newArray = [];
   if (array.length > 0) {
     newArray = array.slice(1);
     return newArray;
-  } else {
-    return undefined;
   }
+  return undefined;
 };
 
-// TEST CODE
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
-
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+module.exports = tail;
